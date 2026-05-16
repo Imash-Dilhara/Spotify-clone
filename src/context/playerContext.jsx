35a64518d0dc9997f3d0dc8a775b389131ffc94a
Imash-Dilhara <imashdilhara1225@gmail.com>
2026-05-16 09:57:31 +0530

    Play selected music track

--- src/context/playerContext.jsx
+++ src/context/playerContext.jsx
@@ -32,6 +32,12 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false);
     }
 
+    const playWithId = async(id) =>{
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
     useEffect(() => {
         setTimeout(() => {
             audioRef.current.ontimeupdate = () => {
@@ -62,7 +68,8 @@ const PlayerContextProvider = (props) => {
         setPlayStatus,
         time,
         setTime,
-        play,pause
+        play,pause,
+        playWithId
     }
 
     return(
